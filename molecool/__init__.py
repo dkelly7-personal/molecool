6bd63a2d22f16612b35a8f587ab5fcb2225e93ef
David Kelly <dkelly7@alumni.nd.edu>
2020-12-16 21:34:44 -0500

    Updates and Testing Script

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,7 +4,12 @@ A Python package for analyzing and visualizing xyz files.  For MolSSI Workshop P
 """
 
 # Add imports here
-from .functions import *
+from .functions import canvas
+from .measure import calculate_distance,calculate_angle
+from .visualize import draw_molecule,bond_histogram
+from .molecule import build_bond_list
+
+from . import IO
 
 # Handle versioneer
 from ._version import get_versions
